1c38baf6e5c911ac85fdae7f7c7d07c059685cc7
Fizoratti <robson.felipe@acad.pucrs.br>
2019-08-21 05:33:58 -0300

    Simplify the base code

--- src/App.java
+++ src/App.java
@@ -5,16 +5,7 @@ import java.util.List;
 public class App {
     public static void main(String args[]) {
 
-        Message message = new Message("Hello World!");
-        System.out.println(message.getMessage());
-
-        Scanner ler = new Scanner(System.in);
-
-        String texto = ler.next();
-
-        ler.close();
-
-        System.out.println(texto);
+        Message message = new Message("Works!");
 
     }
 }
